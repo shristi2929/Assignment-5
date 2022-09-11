f584bf78003a3086bf747e863779924af077e536
shristi2929 <95220373+shristi2929@users.noreply.github.com>
2022-09-11 17:21:42 +0530

    Add files via upload

---
+++ Assignment5.py Edyoda.py
@@ -0,0 +1,10 @@
+# (Assignment 5)Write a Python class to implement pow(x, n).
+
+
+class python:
+    def power(x,n):
+        z=pow(x,n)
+        return z
+x=int(input("Enter the Number :"))
+n=int(input("Enter the power :"))
+print(python.power(x,n))
